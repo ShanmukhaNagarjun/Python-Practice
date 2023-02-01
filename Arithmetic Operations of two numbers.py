6b8ca44e324b69d4c2a0e9444ffff0879400617d
ShanmukhaNagarjun <113223833+ShanmukhaNagarjun@users.noreply.github.com>
2023-02-01 15:45:07 +0530

    Add files via upload

---
+++ Arithmetic Operations of two numbers.py
@@ -0,0 +1,11 @@
+# addition, subtraction, multiplication, division, modulus, exponentiation and floor division of the given two numbers
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("The value of", x, "+", y, "is: ", x+y)
+print("The value of", x, "-", y, "is: ", x-y)
+print("The value of", x, "*", y, "is: ", x*y)
+print("The value of", x, "/", y, "is: ", x/y)
+print("The value of", x, "%", y, "is: ", x%y)
+print("The value of", x, "**", y, "is: ", x**y)
+print("The value of", x, "//", y, "is: ", x//y)
